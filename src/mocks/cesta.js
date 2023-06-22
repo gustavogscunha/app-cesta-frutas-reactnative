a3c11fb6c744bdabf4b2ebf95f67657f38bfa697
Monlight7777 <gustavo.soares91@icloud.com>
2023-06-21 22:25:24 -0300

    first commit

---
+++ src/mocks/cesta.js
@@ -0,0 +1,44 @@
+import logo from '../../assets/logo.png';
+import tomate from "../../assets/frutas/Tomate.png";
+import brocolis from "../../assets/frutas/Brócolis.png";
+import batata from "../../assets/frutas/Batata.png";
+import pepino from "../../assets/frutas/Pepino.png";
+
+
+const cesta = {
+    topo: {
+        titulo: "Detalhe da cesta",
+    },
+    detalhes: {
+        nome: "Cesta de Verduras",
+        logoFazenda: logo,
+        nomeFazenda: "Jenny Jack Farm",
+        descricao: "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha.",
+        preco: "R$40,00",
+        botao: "Comprar",
+    },
+
+    itens: {
+        titulo: "Itens da cesta",
+        lista: [
+            {
+                nome: "Tomate",
+                imagem: tomate,
+            },
+            {
+                nome: "Brócolis",
+                imagem: brocolis,
+            },
+            {
+                nome: "Batata",
+                imagem: batata,
+            },
+            {
+                nome: "Pepino",
+                imagem: pepino,
+            },
+        ]
+    }
+}
+
+export default cesta;
